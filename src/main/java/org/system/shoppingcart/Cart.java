f58f14e7f13af6063adea7188189fe0b1d60ae12
Shiraz Shrestha <shirazshrestha@outlook.com>
2020-04-29 15:43:36 -0700

    Shopping Cart Module added

---
+++ src/main/java/org/system/shoppingcart/Cart.java
@@ -0,0 +1,38 @@
+package org.system.shoppingcart;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Line> lines;
+
+    public void addLine(Line line) {
+        if (lines == null) {
+            lines = new ArrayList<>();
+            lines.add(line);
+        }
+    }
+
+    public Double getTotal() {
+        Double total = 0.0;
+        for (Line line : lines) {
+            total += line.getPrice();
+        }
+        return total;
+    }
+
+
+}
---
+++ src/main/java/org/system/shoppingcart/Line.java
@@ -0,0 +1,26 @@
+package org.system.shoppingcart;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.system.product.model.Product;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Product product;
+
+    public Double getPrice(){
+        return quantity * product.getPrice();
+    }
+}
