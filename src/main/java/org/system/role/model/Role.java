2bb661f7d3a2b2f52a84a3ce142f990c92d2ec9d
Mauro Travieso <mauro_travieso@hotmail.com>
2020-05-04 12:29:22 -0500

    Relationed changes

--- src/main/java/org/system/role/model/Role.java
+++ src/main/java/org/system/role/model/Role.java
@@ -43,4 +43,9 @@ public class Role {
     public void setUsers(List<User> users) {
         this.users = users;
     }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
 }
